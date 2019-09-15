3cdfaf47143920cc54f40f10fca4052e42115cc9
polsevev <54860401+polsevev@users.noreply.github.com>
2019-09-15 05:25:16 +0200

    Add files via upload

---
+++ anotherbrick.py
@@ -0,0 +1,13 @@
+a = input()
+f = a.split(" ")
+height = int(f[0])
+width = int(f[1])
+numberOfBricks = int(f[2])
+x = input()
+widthOfBricks = x.split(" ")
+
+for n in range(0, height):
+    if width % wid
+    for d in range(0, width):
+
+
